6538ed89fb391993de46fdc204f56cfcddf4e22d
DsLiner <rgdkdlel4@naver.com>
2016-07-10 23:42:05 +0900

    UART_test ver.0.0.1
    UART test basic

---
+++ UART_test/Debug/Application/SW4STM32/subdir.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f407xx.s 
+
+OBJS += \
+./Application/SW4STM32/startup_stm32f407xx.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Application/SW4STM32/startup_stm32f407xx.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f407xx.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Assembler'
+	@echo %cd%
+	arm-none-eabi-as -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -g -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ UART_test/Debug/Drivers/STM32F4xx_HAL_Driver/subdir.mk
@@ -0,0 +1,179 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c \
+C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c 
+
+OBJS += \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_cortex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ramfunc.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_gpio.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim_ex.o \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_uart.o 
+
+C_DEPS += \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_cortex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ramfunc.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_gpio.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim_ex.d \
+./Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_cortex.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_cortex.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma_ex.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma_ex.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ex.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ex.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ramfunc.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ramfunc.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_gpio.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_gpio.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr_ex.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_pwr_ex.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc_ex.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_rcc_ex.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim_ex.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_tim_ex.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_uart.o: C:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/SW4STM32 workspace/UART/Inc" -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Include -IC:/Users/DsLiner/STM32Cube/Repository/STM32Cube_FW_F4_V1.12.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_uart.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
